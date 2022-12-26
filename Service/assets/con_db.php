f7d8bc42e06067c147adc5262ee45ad06468682b
Jorge Gomez <50585560+donis-32@users.noreply.github.com>
2022-12-26 13:35:51 -0600

    Update con_db.php

--- Service/assets/con_db.php
+++ Service/assets/con_db.php
@@ -1,8 +1,8 @@
 <?php
-	$servername = "sql872.main-hosting.eu";
-	$username = "u474179435_admin_laats";
-	$password = "wxJ]E9]X";
-	$db = "u474179435_db_laats";
+	$servername = "###";
+	$username = "###";
+	$password = "###";
+	$db = "###";
 // Create connection
 $conex = mysqli_connect($servername, $username, $password,$db);
 
